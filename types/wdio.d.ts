7d68ff85562f8a790b3d5b2b1c2f2f379b34597b
Vojtěch Červený <cervik49@gmail.com>
2021-04-08 18:03:48 +0200

    chore: :art: customCommands support typescript
    boilerplate how to create types for custom wdio commands

---
+++ types/wdio.d.ts
@@ -0,0 +1,16 @@
+export {}
+declare global {
+  namespace WebdriverIO {
+    interface Browser {
+      browserCustomCommand: (arg: any) => void
+    }
+
+    interface MultiRemoteBrowser {
+      browserCustomCommand: (arg: any) => void
+    }
+
+    interface Element {
+      elementCustomCommand: (arg: any) => number
+    }
+  }
+}
